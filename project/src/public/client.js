7601dadf39353f7b16730b56ebefab245f5c3980
Didier.Grand <didier.grand@scott-sports.com>
2022-07-18 16:40:10 +0200

    Get images data and display them the into te ui

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,8 +1,9 @@
-let store = {
-    user: { name: "Student" },
+let store = Immutable.Map({
+    user: Immutable.Map({ name: 'Student' }),
     apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+    rovers: Immutable.List(['curiosity', 'opportunity', 'spirit']),
+    currentRover: ''
+})
 
 // add our markup to the page
 const root = document.getElementById('root')
@@ -29,7 +30,10 @@ const App = (state) => {
         <main>
             <section>
                 <!-- rover infos -->
-                <!-- rover images -->
+                <div class="rover_datas">${roverDatas(state)}</div>
+                <!-- rover images -->                
+                <div class="rover_images">${roverImages(state)}</div>
+
             </section>
         </main>
         <footer>
@@ -45,77 +49,51 @@ window.addEventListener('load', () => {
 
 // ----- COMPONENTS ----- //
 
-// create rover tabs --> <li> with rover name
+// create list with rovers
 
 const createRoverTabs = (state) => {
-    const map1 = state.rovers.map(rover => '<li id='+rover+' onclick="getRoverData(event)">'+rover+'</li>').join('');
+    const map1 = state.get('rovers').map(rover => '<li id='+rover+' onclick="selectedRover(event)">'+rover+'</li>').join('');
     return map1
 }
 // manage tab state
 
-// get rover images
 
-// add images to content
+// Get rover datas
+const roverDatas = (state) => {
+    const currentRover = state.get('currentRover');
+    if(currentRover){
+        return `
+        <p>Rover: ${currentRover.latest_photos[0].rover.name}</p>
+        <p>State: ${currentRover.latest_photos[0].rover.status}</p>
+        <p>Launch date: ${currentRover.latest_photos[0].rover.launch_date}</p>
+        <p>Landing date: ${currentRover.latest_photos[0].rover.landing_date}</p>
+        `
+    }
+}
+// Get rover images
+const roverImages = (state) => {
+    const currentRover = state.get('currentRover');
+    if(currentRover){
+        return currentRover.latest_photos.map( r => 
+            `<img src="${r.img_src}" />`
+        ).join('')
+    }
+}
 
 // ----- EVENTS ----- //
 
 // tabs click event
-const getRoverData = event =>{
-    console.log(event.target.id);
+const selectedRover = event =>{
+    getRoverData(event.target.id,store)
 }
 
+// ------------------------------------------------------  API CALL
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
-
-
-
-// ------------------------------------------------------  API CALLS
-
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-    console.log(data);
-    return data
-}
-
-const getRoverImages = async (roverName, state) => {
+const getRoverData = async (roverName, state) => {
     let { currentRover } = state
    const response = await fetch(`http://localhost:3000/rovers/${roverName}`) // get data or Response from the promise returned by fetch()
     currentRover = await response.json() 
-
-    // set data from the server to Immutable 'currenRover'
-    const newState = store.set('currentRover', currentRover);
-    // updates the old state with the new information
+    const newState = store.set('currentRover', currentRover)
     updateStore(store, newState)
     return currentRover
 }
